52c43dfb32357ab9123e032bb628c3fcee7632eb
Anabelle <git@anabelle.dev>
2023-07-27 23:11:59 -0400

    checkpoint: these revisions seem to work

--- classscrape.py
+++ classscrape.py
@@ -8,10 +8,54 @@ import pickle
 
 root_url = "ssb.cofc.edu"
 
+def createSession(baseURL,datecode):
+	handshakeURL = f'https://{baseURL}/StudentRegistrationSsb/ssb/term/termSelection?mode=search'
+	auth_session = requests.Session()
+	auth_response = auth_session.get(handshakeURL)
+	auth_response_parser = BeautifulSoup(auth_response.text,'html.parser')
+	auth_token = auth_response_parser.find('meta',{'name':'synchronizerToken'}).attrs['content']
+
+	final_headers = {
+		'Accept':'application/json, text/javascript, */*; q=0.01',
+		'Accept-Encoding':'gzip, deflate, br',
+		'Accept-Language':'en-CA,en-GB;q=0.9,en-US;q=0.8,en;q=0.7',
+		'Cookie':f'JSESSIONID={auth_session.cookies["JSESSIONID"]}',
+		'Connection':'keep-alive',
+		'Host':baseURL,
+		'Referer':(
+			f'https://{baseURL}/StudentRegistrationSsb/ssb/classSearch/classSearch' 
+		),
+		'User-Agent':(
+			'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
+			'(KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36'
+		),
+		'X-Synchronizer-Token':auth_token,
+		'X-Requested-With':'XMLHttpRequest',
+	}
+	
+	provisional_headers = {
+		** final_headers,
+		'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8',
+		'Accept':'*/*',
+	}
+
+	auth_session.post(
+		f'https://{baseURL}/StudentRegistrationSsb/ssb/term/search?mode=search',
+		data=f'term={datecode}&studyPath=&studyPathText=&startDatepicker=&endDatepicker=',
+		headers=provisional_headers
+	)
+
+	subj = None
+
+	second_url = f"https://{root_url}/StudentRegistrationSsb/ssb/searchResults/searchResults?{'txt_subject={subj}&' if subj else ''}txt_term={datecode}&startDatepicker=&endDatepicker=&pageOffset=0&pageMaxSize=100&sortColumn=subjectDescription&sortDirection=asc"
+	final = auth_session.get(second_url,headers=final_headers)
+	return json.loads(final.text)
+
+
 def make_3_magic_requests(datecode,subj=False):
 	first_url = f'https://{root_url}/StudentRegistrationSsb/ssb/term/termSelection?mode=search'
 
-	second_url = f"https://{root_url}/StudentRegistrationSsb/ssb/searchResults/searchResults?{'txt_subject={subj}&' if subj else ''}txt_term={datecode}&startDatepicker=&endDatepicker=&pageOffset=0&pageMaxSize=100&sortColumn=subjectDescription&sortDirection=asc"
+	
 
 	header = {
 		'Accept':'application/json, text/javascript, */*; q=0.01',
@@ -56,4 +100,4 @@ def make_3_magic_requests(datecode,subj=False):
 
 if __name__ == "__main__":
 	date = '202310'
-	print(make_3_magic_requests(date))
+	print(createSession(root_url,date))
