077b7e181e4f9107be6a188d57453fe50e37b7df
Anabelle <git@anabelle.dev>
2023-07-27 23:21:27 -0400

    this also works

--- classscrape.py
+++ classscrape.py
@@ -45,59 +45,16 @@ def createSession(baseURL,datecode):
 		headers=provisional_headers
 	)
 
+	auth_session.headers = final_headers
+	return auth_session
+
+def first50classes(auth_session,datecode):
 	subj = None
 
 	second_url = f"https://{root_url}/StudentRegistrationSsb/ssb/searchResults/searchResults?{'txt_subject={subj}&' if subj else ''}txt_term={datecode}&startDatepicker=&endDatepicker=&pageOffset=0&pageMaxSize=100&sortColumn=subjectDescription&sortDirection=asc"
-	final = auth_session.get(second_url,headers=final_headers)
-	return json.loads(final.text)
-
-
-def make_3_magic_requests(datecode,subj=False):
-	first_url = f'https://{root_url}/StudentRegistrationSsb/ssb/term/termSelection?mode=search'
-
-	
-
-	header = {
-		'Accept':'application/json, text/javascript, */*; q=0.01',
-		'Accept-Encoding':'gzip, deflate, br',
-		'Accept-Language':'en-CA,en-GB;q=0.9,en-US;q=0.8,en;q=0.7',
-		'Cookie':'',
-		'Connection':'keep-alive',
-		'Host':root_url,
-		'Referer':(
-			f'{root_url}/StudentRegistrationSsb/ssb/classSearch/classSearch' 
-		),
-		'User-Agent':(
-			'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
-			'(KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36'
-		),
-		'X-Synchronizer-Token':'',
-		'X-Requested-With':'XMLHttpRequest',
-	}
-
-	header2 = {
-		** header,
-		'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8',
-		'Accept':'*/*',
-	}
-
-	my_session = requests.Session()
-
-	first = my_session.get(first_url)
-	soup = BeautifulSoup(first.text,'html.parser')
-	token = soup.find('meta',{'name':'synchronizerToken'}).attrs['content']
-
-	header['X-Synchronizer-Token'] = token
-	header['Cookie'] = 'JSESSIONID'+'='+my_session.cookies['JSESSIONID']
-
-	header2['X-Synchronizer-Token'] = token
-	header2['Cookie'] = 'JSESSIONID'+'='+my_session.cookies['JSESSIONID']
-
-	my_session.post(f'https://{root_url}/StudentRegistrationSsb/ssb/term/search?mode=search',data='term={}&studyPath=&studyPathText=&startDatepicker=&endDatepicker='.format(datecode),headers=header2)
-
-	final = my_session.get(second_url,headers=header)
+	final = auth_session.get(second_url)
 	return json.loads(final.text)
 
 if __name__ == "__main__":
 	date = '202310'
-	print(createSession(root_url,date))
+	print(first50classes(createSession(root_url,date),date))
